a27ac87322f0425a73c3261b60be53a5fa8f0ac8
Irwin Arruda <arruda.irwin@gmail.com>
2023-06-03 21:43:12 -0300

    ✨ Fixed cookies

--- src/routes/room/[id]/+page.server.ts
+++ src/routes/room/[id]/+page.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export function load({ params, cookies }) {
 	if (cookies.get('isPlaying')) {
-		cookies.delete('isPlaying');
+		cookies.delete('isPlaying', { path: '/' });
 		return { id: params.id };
 	}
 	throw redirect(307, `/join?id=${params.id}`);
